1501bf1c8ecdb0391dbd6aca09491345b7d56621
Chirag Manghani <43926813+M-Chirag@users.noreply.github.com>
2022-06-28 17:17:08 +0530

    Time: 26 ms (17.88%), Space: 48.3 MB (23.64%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.java
+++ 36-valid-sudoku/36-valid-sudoku.java
@@ -1,42 +1,21 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
         
-        for(int i=0; i<9 ; i++){
-            int[] countCheckRow = new int[10];
-            int[] countCheckCol = new int[10];
+        HashSet<String> row = new HashSet<>();
+        HashSet<String> col = new HashSet<>();
+        HashSet<String> box = new HashSet<>();
+        
+        for(int i=0; i<9; i++){
             for(int j=0; j<9; j++){
-                if(Character.compare(board[i][j],'.')!=0){
-                    countCheckRow[Integer.parseInt(String.valueOf(board[i][j]))]++;
-                    if(countCheckRow[Integer.parseInt(String.valueOf(board[i][j]))]>1)
+                char val = board[i][j];
+                if(val != '.'){
+                    if(!row.add(i+"row"+board[i][j]) || 
+                       !col.add(j+"col"+board[i][j]) ||
+                       !box.add(i/3+""+j/3+"box"+board[i][j]))
                         return false;
-                }
-                
-                if(Character.compare(board[j][i],'.')!=0){
-                    countCheckCol[Integer.parseInt(String.valueOf(board[j][i]))]++;
-                    if(countCheckCol[Integer.parseInt(String.valueOf(board[j][i]))]>1)
-                        return false;
-                }
-                
-            }
-        }
-        
-        for(int i=0; i<9; i+=3){
-            for(int j=0; j<9; j+=3){
-                int[] countCheckBox = new int[10];
-                for(int k=i;k<i+3;k++){
-                    for(int l=j;l<j+3;l++){
-                    System.out.println(k+""+l);
-                    if(Character.compare(board[k][l],'.')!=0){
-                        countCheckBox[Integer.parseInt(String.valueOf(board[k][l]))]++;
-                            if(countCheckBox[Integer.parseInt(String.valueOf(board[k][l]))]>1)
-                                return false;
-                    }
                 }
             }
         }
-    }
-        
-        
         return true;
     }
 }
