06f85c25b6a45e82e532edc3a42f0ca10e31713b
Chirag Manghani <43926813+M-Chirag@users.noreply.github.com>
2022-06-30 02:04:33 +0530

    Time: 33 ms (79.32%), Space: 60.8 MB (83.33%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -1,26 +1,27 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
+        Set<Integer> numSet = new HashSet<>();
+        
         if(nums.length==0) return 0;
-        Arrays.sort(nums);
-        Queue<Integer> countSet = new PriorityQueue<>((a,b)->b-a);
-        int count = 1;
-        for(int i=0;i< nums.length-1; i++){
-            System.out.println(nums[i]);
-            if(nums[i+1]==nums[i])
-                continue;
-            if(nums[i+1]-nums[i]==1){
-                System.out.println("|"+count);
-                count++;
-            }
-            else{ 
-                System.out.println(count+".......");
-                countSet.add(count);
-                count =1;
-                continue;
+        
+        for(int i=0; i<nums.length; i++){
+            numSet.add(nums[i]);
+        }
+        
+        Queue<Integer> countQ = new PriorityQueue<>((a,b)-> b-a);
+        countQ.add(1);
+        for(Integer n: numSet){
+            int count=1;
+            if(!numSet.contains(n-1)){
+                int seqElement = n+1;
+                while(numSet.contains(seqElement)){
+                    count++;
+                    seqElement++;
+                }
+                countQ.add(count);
             }
         }
-        countSet.add(count);
-        System.out.println(countSet);
-        return countSet.peek();
+        System.out.println(countQ);
+        return countQ.peek();
     }
 }
