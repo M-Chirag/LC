155444633d12f2fcddbce069a0a560375ea1b96e
Chirag Manghani <43926813+M-Chirag@users.noreply.github.com>
2022-10-28 16:18:44 -0700

    Time: 1475 ms (60.43%), Space: 29.4 MB (5.75%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -4,16 +4,12 @@ class Solution:
         stack = [] 
         
         for i,v in enumerate(temperatures):
-            
-            #push to stack if element smaller than stack.top() 
-            if(len(stack)==0 or v<=stack[-1][0]):
-                stack.append((v,i))
-            
             #keep popping and adding to result array until we find an element at stack.top() which is                   greater than v
-            if(len(stack)>0 and v>stack[-1][0]):
-                while(len(stack)>0 and v>stack[-1][0]):
-                    res[stack[-1][1]] = i-stack[-1][1]
-                    stack.pop()
-                stack.append((v,i))
+            while(stack and v>stack[-1][0]):
+                res[stack[-1][1]] = i-stack[-1][1]
+                stack.pop()
+            
+            #push element after the above check is complete
+            stack.append((v,i))
         return res
         
