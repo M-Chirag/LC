dca8ac1e5e85b9534488daaa2f70f20057062030
Chirag Manghani <43926813+M-Chirag@users.noreply.github.com>
2023-05-14 20:04:01 -0400

    Time: 142 ms (41.77%), Space: 50.1 MB (47.74%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        
+        Arrays.sort(nums);
+        
+        for(int i=0; i<nums.length; i++)
+        {
+            int l=i+1;
+            int r=nums.length-1;
+            
+            if(i!=0 && nums[i]==nums[i-1])
+                continue;
+            
+            while(l<r){
+                int threeSum = nums[i]+nums[l]+nums[r];
+                if(0<threeSum){
+                    r--;
+                }else if(0>threeSum){
+                    l++;
+                }else{
+                    List<Integer> candidate = new ArrayList<>();
+                    Collections.addAll(candidate,new Integer[]{nums[i],nums[l],nums[r]});
+                    System.out.println(candidate);
+                    res.add(candidate);
+                    l++;
+                    while(nums[l]==nums[l-1] && l<r)
+                        l++;
+                    }
+            }
+        }
+      // List<List<Integer>> result = res.stream().collect(Collectors.toList());
+      return res;  
+    }
+}
